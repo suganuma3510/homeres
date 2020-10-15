bbd271c482bc21d70a95d3768fca4884669a9462
suganuma3510 <suganuma3510@gmail.com>
2020-10-15 11:56:00 +0900

    add：headerコンポーネント追加

---
+++ client/components/header.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app-bar
+    :collapse="!collapseOnScroll"
+    v-on:mouseover="open"
+    v-on:mouseleave="collapse"
+    app
+    flat
+    color="transparent"
+    dark
+  >
+  <v-container>
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+    <v-toolbar-title>Collapsing Bar</v-toolbar-title>
+</v-container>
+  </v-app-bar>
+  
+</template>
+<script>
+export default {
+  data: () => ({
+    collapseOnScroll: false,
+  }),
+  methods: {
+    open() {
+      this.collapseOnScroll = true;
+    },
+    collapse() {
+      this.collapseOnScroll = false;
+    },
+  },
+};
+</script>
--- client/layouts/default.vue
+++ client/layouts/default.vue
@@ -1,24 +1,13 @@
 <template>
-  <v-app dark>
-    <v-app-bar app color="transparent" dark inverted-scroll>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
-      <v-toolbar-title>Title</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-heart</v-icon>
-      </v-btn>
-      <v-btn icon>
-        <v-icon>mdi-dots-vertical</v-icon>
-      </v-btn>
-    </v-app-bar>
+  <v-app :style="{ background: $vuetify.theme.themes[theme].background }">
+    <Header />
 
     <v-main>
-      <v-content>
-        <nuxt />
-      </v-content>
+      <v-container>
+        <v-content>
+          <nuxt />
+        </v-content>
+      </v-container>
     </v-main>
 
     <v-footer :absolute="!fixed" app>
@@ -28,29 +17,14 @@
 </template>
 
 <script>
+import Header from "~/components/header.vue";
+
 export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: "mdi-apps",
-          title: "Welcome",
-          to: "/",
-        },
-        {
-          icon: "mdi-chart-bubble",
-          title: "Inspire",
-          to: "/inspire",
-        },
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: "Vuetify.js",
-    };
+  name: "App",
+  computed: {
+    theme() {
+      return this.$vuetify.theme.dark ? "dark" : "light";
+    },
   },
 };
 </script>
