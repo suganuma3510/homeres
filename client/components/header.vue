7c7f4675e14fd3524788d766f8cc45b184979f30
suganuma3510 <suganuma3510@gmail.com>
2020-10-27 14:24:17 +0900

    update：ヘッダーのマウスオーバー時の動きを修正

--- client/components/header.vue
+++ client/components/header.vue
@@ -1,33 +1,46 @@
 <template>
   <v-app-bar
     :collapse="!collapseOnScroll"
-    v-on:mouseover="open"
-    v-on:mouseleave="collapse"
-    app
+    fixed
     flat
     color="transparent"
     dark
+    v-bind:class="{ active: collapseOnScroll }"
   >
-  <v-container>
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
-    <v-toolbar-title>Collapsing Bar</v-toolbar-title>
-</v-container>
-  </v-app-bar>
   
+    <v-app-bar-nav-icon x-large v-on:mouseover="open"></v-app-bar-nav-icon>
+    <v-toolbar-title>HOMERES</v-toolbar-title>
+
+    <v-spacer></v-spacer>
+    <v-toolbar-title>
+      <v-btn icon>
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+    </v-toolbar-title>
+
+    <v-btn icon  v-if="collapseOnScroll" >
+      <v-icon v-on:click="close">mdi-send mdi-rotate-180</v-icon>
+    </v-btn>
+  </v-app-bar>
 </template>
+
 <script>
 export default {
   data: () => ({
-    collapseOnScroll: false,
+    collapseOnScroll: true,
   }),
   methods: {
     open() {
       this.collapseOnScroll = true;
     },
-    collapse() {
+    close() {
       this.collapseOnScroll = false;
     },
   },
 };
 </script>
+<style>
+.active {
+  margin: 0 8vw;
+}
+</style>
