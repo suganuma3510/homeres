7e99b639a336ce0a6091d715e52072c49b5c8051
suganuma3510 <suganuma3510@gmail.com>
2020-11-18 06:43:26 +0900

    add：リクエストに対してJSON形式で飲食店情報を返す処理を追加

--- server/go.mod
+++ server/go.mod
@@ -1,10 +1,11 @@
-module github.com/suganuma3510/project_name
+module github.com/suganuma3510/homeres
 
 go 1.15
 
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/go-sql-driver/mysql v1.5.0
+	github.com/joho/godotenv v1.3.0
 	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/sirupsen/logrus v1.7.0
---
+++ server/interface/controller/shop.go
@@ -0,0 +1,34 @@
+package controller
+
+import (
+	"github.com/labstack/echo"
+	"github.com/suganuma3510/homeres/service"
+	"net/http"
+)
+
+// ShopController : ShopにおけるControllerのインターフェース
+type ShopController interface {
+	GetShopList(e echo.Context) error
+}
+
+type shopController struct {
+	shopService service.ShopService
+}
+
+// NewShopService : Shopデータに関するControllerを生成
+func NewShopController(ss service.ShopService) ShopController {
+	return &shopController{
+		shopService: ss,
+	}
+}
+
+// GetShopList : Shopデータのリストを取得し返却
+func (sc shopController) GetShopList(c echo.Context) error {
+
+	ctx := c.Request().Context()
+	shops, err := sc.shopService.GetShopList(ctx)
+	if err != nil {
+		return c.String(http.StatusBadRequest, "Validate is failed: "+err.Error())
+	}
+	return c.JSON(http.StatusOK, shops)
+}
