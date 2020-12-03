f70df0575df4fe7b08bf5371b2920298014d9508
suganuma3510 <suganuma3510@gmail.com>
2020-12-03 19:10:24 +0900

    add：storeのshopsのテストを追加

--- client/test/store/shops.spec.js
+++ client/test/store/shops.spec.js
@@ -1,23 +1,21 @@
 import Vuex from 'vuex'
 import * as shops from '@/store/shops'
-import {
-  createLocalVue
-} from '@vue/test-utils'
+import { createLocalVue } from '@vue/test-utils'
 
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
-describe('store/index.js', () => {
+describe('store/shops.js', () => {
   let store
-  let shops1, shops2
+  let shop1, shop2
   beforeEach(() => {
     store = new Vuex.Store(shops)
-    shops1 = {
+    shop1 = {
       id: '1',
       name: 'name_1',
       category: 'category_1'
     }
-    shops2 = {
+    shop2 = {
       id: '2',
       name: 'name_2',
       category: 'category_2'
@@ -27,16 +25,31 @@ describe('store/index.js', () => {
   describe('getters', () => {
     let shops
     beforeEach(() => {
-      shops = [shops1, shops2]
+      shops = [shop1, shop2]
       store.replaceState({
-        shops: shops
+        shops: shops,
+        shop: shop1
       })
     })
 
     describe('shops', () => {
       test('statusがshopsのshopsが取得できること', () => {
-        expect(store.getters['shops']).toContainEqual(shops1)
-        expect(store.getters['shops']).toContainEqual(shops2)
+        expect(store.getters['shops']).toContainEqual(shop1)
+        expect(store.getters['shops']).toContainEqual(shop2)
+      })
+    })
+
+    describe('shops_all', () => {
+      test('すべてのshopsが取得できること', () => {
+        expect(store.getters['shops']).toEqual(
+          expect.arrayContaining(shops)
+        )
+      })
+    })
+
+    describe('shop', () => {
+      test('shopが取得できること', () => {
+        expect(store.getters['shop']).toBe(shop1)
       })
     })
   })
